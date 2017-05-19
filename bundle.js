b7e88b572ffd0d938399679e70dfdcce47c748df
cornhundred <nickfloresfernandez@gmail.com>
2017-05-19 10:08:44 -0400

    exported main function as global variable (hacky) and loading in pixel size from file

--- bundle.js
+++ bundle.js
@@ -1,145 +1,154 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-//////////////////////////////////////////////////
-// smoothly animate points
-//////////////////////////////////////////////////
-const glsl = require('glslify')
-const linspace = require('ndarray-linspace')
-const vectorFill = require('ndarray-vector-fill')
-const ndarray = require('ndarray')
-const ease = require('eases/cubic-in-out')
-const regl = require('regl')()
+// // pass filename and run externally
+// //////////////////////////////////
+// var filename = 'data/scores.csv'
+// smoothly_animate(filename);
 
-var something = {thing:'something'}
+window.smoothly_animate = smoothly_animate;
 
-require('resl')({
-  manifest:{
-    'scores':{
-      type: 'text',
-      src: 'data/scores.csv'
-    }
-  },
-  onDone: (assets) => {
+function smoothly_animate(filename){
 
-    // require('regl')({
-    //   onDone: require('fail-nicely')(run)
-    // })
+  console.log('passing ' + filename + ' to smoothly_animate')
 
-    run(regl, something, assets);
+  //////////////////////////////////////////////////
+  // smoothly animate points
+  //////////////////////////////////////////////////
+  const glsl = require('glslify')
+  const linspace = require('ndarray-linspace')
+  const vectorFill = require('ndarray-vector-fill')
+  const ndarray = require('ndarray')
+  const ease = require('eases/cubic-in-out')
+  const regl = require('regl')()
 
-  }
-})
+  var something = {thing:'something'}
 
-// // original way of running
-// ///////////////////////////
-// require('regl')({
-//   onDone: require('fail-nicely')(run)
-// })
 
-// // run and pass argument
-// /////////////////////////
-// run(regl, something);
-
-function run (regl, new_thing, assets) {
-
-  // debugger
-
-  console.log('running')
-  console.log(new_thing)
-  console.log(assets.scores)
-
-  let max_nodes = 1000
-  let n = max_nodes/10
-  let datasets = []
-  let colorBasis
-  let datasetPtr = 0
-
-  let pointRadius = 4
-
-  let lastSwitchTime = 0
-  let switchInterval = 5
-  let switchDuration = 3
-
-  const createDatasets = () => {
-    datasets = [phyllotaxis, grid].map((func, i) =>
-      (datasets[i] || regl.buffer)(vectorFill(ndarray([], [n, 2]), func(n)))
-    )
-  }
-
-  // Initialize:
-  createDatasets()
-
-  // Create nice controls:
-  require('control-panel')([
-    {type: 'range', min: 10, max: max_nodes/2, label: 'n', initial: n, step: 50}
-  ], {width: 400}).on('input', (data) => {
-    if (data.n !== n) {
-      n = Math.round(data.n)
-      createDatasets()
-    }
-  })
-
-  const drawPoints = regl({
-    vert: `
-      precision mediump float;
-      attribute vec2 xy0, xy1;
-      attribute float;
-      varying float t;
-      uniform float aspect, interp, radius;
-      void main () {
-        // Interpolate between the two positions:
-        vec2 pos = mix(xy0, xy1, interp);
-        gl_Position = vec4(pos.x, pos.y * aspect, 0, 1);
-        gl_PointSize = radius;
+  require('resl')({
+    manifest:{
+      'scores':{
+        type: 'text',
+        src: filename
       }
-    `,
-    frag: glsl(["\n      precision mediump float;\n#define GLSLIFY 1\n\n      varying float t;\n      void main () {\n        gl_FragColor = vec4(0, 0, 0, 1);\n      }\n    "]),
-    depth: {enable: false},
-    attributes: {
-      // Pass two buffers between which we ease in the vertex shader:
-      xy0: () => datasets[datasetPtr % datasets.length],
-      xy1: () => datasets[(datasetPtr + 1) % datasets.length],
     },
-    uniforms: {
-      radius: () => pointRadius,
-      aspect: ctx => ctx.viewportWidth / ctx.viewportHeight,
-      // The current interpolation position, from 0 to 1:
-      interp: (ctx, props) => Math.max(0, Math.min(1, props.interp))
-    },
-    primitive: 'point',
-    count: () => n
+    onDone: (assets) => {
+      run(regl, something, assets);
+    }
   })
 
-  regl.frame(({time}) => {
-    // Check how long it's been since the last switch, and cycle the buffers
-    // and reset the timer if it's time for a switch:
-    if ((time - lastSwitchTime) > switchInterval) {
-      lastSwitchTime = time
-      datasetPtr++
+  // // original way of running
+  // ///////////////////////////
+  // require('regl')({
+  //   onDone: require('fail-nicely')(run)
+  // })
+
+  // // run and pass argument
+  // /////////////////////////
+  // run(regl, something);
+
+  function run (regl, new_thing, assets) {
+
+    // debugger
+
+
+    console.log('running')
+    console.log(new_thing)
+    console.log(assets.scores)
+
+    let max_nodes = 500
+    let n = max_nodes/10
+    let datasets = []
+    let colorBasis
+    let datasetPtr = 0
+
+    let pointRadius = parseInt(assets.scores)
+
+    let lastSwitchTime = 0
+    let switchInterval = 5
+    let switchDuration = 3
+
+    const createDatasets = () => {
+      datasets = [phyllotaxis, grid].map((func, i) =>
+        (datasets[i] || regl.buffer)(vectorFill(ndarray([], [n, 2]), func(n)))
+      )
     }
 
-    drawPoints({interp: ease((time - lastSwitchTime) / switchDuration)})
-  })
-}
+    // Initialize:
+    createDatasets()
+
+    // Create nice controls:
+    require('control-panel')([
+      {type: 'range', min: 10, max: max_nodes/2, label: 'n', initial: n, step: 50}
+    ], {width: 400}).on('input', (data) => {
+      if (data.n !== n) {
+        n = Math.round(data.n)
+        createDatasets()
+      }
+    })
+
+    const drawPoints = regl({
+      vert: `
+        precision mediump float;
+        attribute vec2 xy0, xy1;
+        attribute float;
+        varying float t;
+        uniform float aspect, interp, radius;
+        void main () {
+          // Interpolate between the two positions:
+          vec2 pos = mix(xy0, xy1, interp);
+          gl_Position = vec4(pos.x, pos.y * aspect, 0, 1);
+          gl_PointSize = radius;
+        }
+      `,
+      frag: glsl(["\n        precision mediump float;\n#define GLSLIFY 1\n\n        varying float t;\n        void main () {\n          gl_FragColor = vec4(0, 0, 0, 1);\n        }\n      "]),
+      depth: {enable: false},
+      attributes: {
+        // Pass two buffers between which we ease in the vertex shader:
+        xy0: () => datasets[datasetPtr % datasets.length],
+        xy1: () => datasets[(datasetPtr + 1) % datasets.length],
+      },
+      uniforms: {
+        radius: () => pointRadius,
+        aspect: ctx => ctx.viewportWidth / ctx.viewportHeight,
+        // The current interpolation position, from 0 to 1:
+        interp: (ctx, props) => Math.max(0, Math.min(1, props.interp))
+      },
+      primitive: 'point',
+      count: () => n
+    })
+
+    regl.frame(({time}) => {
+      // Check how long it's been since the last switch, and cycle the buffers
+      // and reset the timer if it's time for a switch:
+      if ((time - lastSwitchTime) > switchInterval) {
+        lastSwitchTime = time
+        datasetPtr++
+      }
+
+      drawPoints({interp: ease((time - lastSwitchTime) / switchDuration)})
+    })
+  }
 
 
-function phyllotaxis (n) {
-  const theta = Math.PI * (3 - Math.sqrt(5))
-  return function (i) {
-    let r = Math.sqrt(i / n)
-    let th = i * theta
-    return [
-      r * Math.cos(th),
-      r * Math.sin(th)
+  function phyllotaxis (n) {
+    const theta = Math.PI * (3 - Math.sqrt(5))
+    return function (i) {
+      let r = Math.sqrt(i / n)
+      let th = i * theta
+      return [
+        r * Math.cos(th),
+        r * Math.sin(th)
+      ]
+    }
+  }
+
+  function grid (n) {
+    const rowlen = Math.round(Math.sqrt(n))
+    return (i) => [
+      -0.8 + 1.6 / rowlen * (i % rowlen),
+      -0.8 + 1.6 / rowlen * Math.floor(i / rowlen)
     ]
   }
-}
 
-function grid (n) {
-  const rowlen = Math.round(Math.sqrt(n))
-  return (i) => [
-    -0.8 + 1.6 / rowlen * (i % rowlen),
-    -0.8 + 1.6 / rowlen * Math.floor(i / rowlen)
-  ]
 }
 
 },{"control-panel":15,"eases/cubic-in-out":27,"glslify":29,"ndarray":52,"ndarray-linspace":50,"ndarray-vector-fill":51,"regl":57,"resl":58}],2:[function(require,module,exports){
