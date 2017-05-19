0ca2f3ecb37de3d6b55400c9e2b0bf2e8d0875ff
cornhundred <nickfloresfernandez@gmail.com>
2017-05-18 21:42:12 -0400

    simplifying smoothly animating example

--- main.js
+++ main.js
@@ -1,76 +1,109 @@
-const regl = require('regl')()
-const mouse = require('mouse-change')()
-const camera = require('regl-camera')(regl)
-const mesh = require('bunny')
-const normals = require('angle-normals')
+//////////////////////////////////////////////////
+// smoothly animate points
+//////////////////////////////////////////////////
+const glsl = require('glslify')
+const linspace = require('ndarray-linspace')
+const vectorFill = require('ndarray-vector-fill')
+const ndarray = require('ndarray')
+const ease = require('eases/cubic-in-out')
 
-require('resl')({
-  manifest:{
-    'scores':{
-      type:'text',
-      src:'data/scores.csv'
-    }
-  },
-  onDone: (assets) => {
-    console.log(assets.scores)
+require('regl')({onDone: require('fail-nicely')(run)})
+
+function run (regl) {
+
+  let max_nodes = 10000
+
+  max_nodes = 0.5 * max_nodes
+
+  let n = max_nodes
+  let datasets = []
+  // let colorBasis
+  let datasetPtr = 0
+
+  let pointRadius = 3
+
+  let lastSwitchTime = 0
+  let switchInterval = 10
+  let switchDuration = 10
+
+  const createDatasets = () => {
+    datasets = [phyllotaxis, grid].map((func, i) =>
+      (datasets[i] || regl.buffer)(vectorFill(ndarray([], [n, 2]), func(n)))
+    )
+    // This is just a list from 1 to 0 for coloring:
+    // colorBasis = (colorBasis || regl.buffer)(linspace(ndarray([], [n]), 1, 0))
   }
-})
 
-console.log('here')
-g_regal = regl;
+  // Initialize:
+  createDatasets()
 
-function processMesh (mesh){
-
-  return regl({
-
-    frag: `
-      precision highp float;
-      varying vec3 color;
-      void main () {
-        gl_FragColor = vec4(color, 1);
-      }
-    `,
-
+  const drawPoints = regl({
     vert: `
-      precision highp float;
-      varying vec3 color;
-      attribute vec3 position, normal;
-      uniform vec2 translate;
-      uniform mat4 projection, view;
-      uniform float t;
-      void main() {
-        color = 0.5 * (1. + normal);
-        gl_Position = projection * view * vec4(position , 1);
+      precision mediump float;
+      attribute vec2 xy0, xy1;
+      attribute float;
+      varying float t;
+      uniform float aspect, interp, radius;
+      void main () {
+        // Interpolate between the two positions:
+        vec2 pos = mix(xy0, xy1, interp);
+        gl_Position = vec4(pos.x, pos.y * aspect, 0, 1);
+        gl_PointSize = radius;
       }
     `,
-
-
+    frag: glsl(`
+      precision mediump float;
+      #pragma glslify: colormap = require(glsl-colormap/viridis)
+      varying float t;
+      void main () {
+        gl_FragColor = vec4( 0, 0, 0, 1);
+      }
+    `),
+    depth: {enable: false},
     attributes: {
-      position: mesh.positions,
-      normal: normals(mesh.cells, mesh.positions)
+      // Pass two buffers between which we ease in the vertex shader:
+      xy0: () => datasets[datasetPtr % datasets.length],
+      xy1: () => datasets[(datasetPtr + 1) % datasets.length],
     },
-
     uniforms: {
-      t: ({tick}) => Math.cos(0.0000001 * tick)
+      radius: () => pointRadius,
+      aspect: ctx => ctx.viewportWidth / ctx.viewportHeight,
+      // The current interpolation position, from 0 to 1:
+      interp: (ctx, props) => Math.max(0, Math.min(1, props.interp))
     },
-
-    elements: mesh.cells
-
+    primitive: 'point',
+    count: () => n
   })
 
+  regl.frame(({time}) => {
+    // Check how long it's been since the last switch, and cycle the buffers
+    // and reset the timer if it's time for a switch:
+    if ((time - lastSwitchTime) > switchInterval) {
+      lastSwitchTime = time
+      datasetPtr++
+    }
 
+    drawPoints({interp: ease((time - lastSwitchTime) / switchDuration)})
+  })
 }
 
-const drawMesh = processMesh(mesh)
 
-regl.frame(() => {
-  regl.clear({
-    color: [0, 0, 0, 1],
-    depth: 1
-  })
+function phyllotaxis (n) {
+  const theta = Math.PI * (3 - Math.sqrt(5))
+  return function (i) {
+    let r = Math.sqrt(i / n)
+    let th = i * theta
+    return [
+      r * Math.cos(th),
+      r * Math.sin(th)
+    ]
+  }
+}
 
-  camera(() => {
-    drawMesh()
-  })
-
-})
+function grid (n) {
+  const rowlen = Math.round(Math.sqrt(n))
+  return (i) => [
+    -0.8 + 1.6 / rowlen * (i % rowlen),
+    -0.8 + 1.6 / rowlen * Math.floor(i / rowlen)
+  ]
+}
---
+++ resl_load_scores.js
@@ -0,0 +1,76 @@
+regl = require('regl')()
+const mouse = require('mouse-change')()
+const camera = require('regl-camera')(regl)
+const mesh = require('bunny')
+const normals = require('angle-normals')
+
+require('resl')({
+  manifest:{
+    'scores':{
+      type:'text',
+      src:'data/scores.csv'
+    }
+  },
+  onDone: (assets) => {
+    console.log(assets.scores)
+  }
+})
+
+console.log('here')
+g_regal = regl;
+
+function processMesh (mesh){
+
+  return regl({
+
+    frag: `
+      precision highp float;
+      varying vec3 color;
+      void main () {
+        gl_FragColor = vec4(color, 1);
+      }
+    `,
+
+    vert: `
+      precision highp float;
+      varying vec3 color;
+      attribute vec3 position, normal;
+      uniform vec2 translate;
+      uniform mat4 projection, view;
+      uniform float t;
+      void main() {
+        color = 0.5 * (1. + normal);
+        gl_Position = projection * view * vec4(position , 1);
+      }
+    `,
+
+
+    attributes: {
+      position: mesh.positions,
+      normal: normals(mesh.cells, mesh.positions)
+    },
+
+    uniforms: {
+      t: ({tick}) => Math.cos(0.0000001 * tick)
+    },
+
+    elements: mesh.cells
+
+  })
+
+
+}
+
+const drawMesh = processMesh(mesh)
+
+regl.frame(() => {
+  regl.clear({
+    color: [0, 0, 0, 1],
+    depth: 1
+  })
+
+  camera(() => {
+    drawMesh()
+  })
+
+})
