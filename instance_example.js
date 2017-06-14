5781e019dd556d5c8b090a80ea42f41069aed061
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-06-14 15:30:22 -0400

    working on instance example

--- instance_example.js
+++ instance_example.js
@@ -1,15 +1,37 @@
 /*
   tags: instancing, basic
 
-  <p> In this example, it is shown how you can draw a bunch of triangles using the
-  instancing feature of regl. </p>
+  In this example, it is shown how you can draw a bunch of triangles using the
+  instancing feature of regl.
+
+  gitter info
+
+  "yeah, instancing uses gpu hardware
+  batching reduces number of bind state changes, but still requires multiple draw calls
+  instancing reuses the same draw call, but requires an extension"
+
+  Mikola Lysenko @mikolalysenko 15:24
+  in general yes
+
+  Nicolas Fernandez @cornhundred 15:24
+  thanks
+
+  Mikola Lysenko @mikolalysenko 15:24
+  as always there are caveats and special cases....
+
+  Nicolas Fernandez @cornhundred 15:25
+  okay
+
+  Mikola Lysenko @mikolalysenko 15:27
+  but instancing is generally < draw calls/vertex processing which means better per
+
  */
 const regl = require('regl')({extensions: ['angle_instanced_arrays']})
 
-var N = 10 // N triangles on the width, N triangles on the height.
+var num_tri = 10 // N triangles on the width, N triangles on the height.
 
 var angle = []
-for (var i = 0; i < N * N; i++) {
+for (var i = 0; i < num_tri * num_tri; i++) {
   // generate random initial angle.
   angle[i] = Math.random() * (2 * Math.PI)
 }
@@ -55,9 +77,9 @@ const draw = regl({
 
     offset: {
       buffer: regl.buffer(
-        Array(N * N).fill().map((_, i) => {
-          var x = -1 + 2 * Math.floor(i / N) / N + 0.1
-          var y = -1 + 2 * (i % N) / N + 0.1
+        Array(num_tri * num_tri).fill().map((_, i) => {
+          var x = -1 + 2 * Math.floor(i / num_tri) / num_tri + 0.1
+          var y = -1 + 2 * (i % num_tri) / num_tri + 0.1
           return [x, y]
         })),
       divisor: 1 // one separate offset for every triangle.
@@ -65,9 +87,9 @@ const draw = regl({
 
     color: {
       buffer: regl.buffer(
-        Array(N * N).fill().map((_, i) => {
-          var r = Math.floor(i / N) / N
-          var g = (i % N) / N
+        Array(num_tri * num_tri).fill().map((_, i) => {
+          var r = Math.floor(i / num_tri) / num_tri
+          var g = (i % num_tri) / num_tri
           return [r, g, r * g + 0.2]
         })),
       divisor: 1 // one separate color for every triangle
@@ -87,7 +109,7 @@ const draw = regl({
   // However, every such triangle are drawn N * N times,
   // through instancing.
   count: 3,
-  instances: N * N
+  instances: num_tri * num_tri
 })
 
 regl.frame(function () {
@@ -96,7 +118,7 @@ regl.frame(function () {
   })
 
   // rotate the triangles every frame.
-  for (var i = 0; i < N * N; i++) {
+  for (var i = 0; i < num_tri * num_tri; i++) {
     angle[i] += 0.01
   }
   angleBuffer.subdata(angle)
