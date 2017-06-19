c1f88d551c1e43dc7d9a73d87cdb31a8e852793a
cornhundred <nickfloresfernandez@gmail.com>
2017-06-19 17:27:04 -0400

    working on text-triangles

--- text-triangles.js
+++ text-triangles.js
@@ -15,15 +15,38 @@ text_vect = vectorizeText('something!', {
 
 var N = 10 // N triangles on the width, N triangles on the height.
 
-// offset_array = Array(N * N).fill().map((_, i) => {
-//           var x = -1 + 2 * Math.floor(i / N) / N + 0.1
-//           var y = -1 + 2 * (i % N) / N + 0.1
-//           return [x, y]
-//         });
+tri_verts = [];
+text_vect.cells.forEach(
+  function(inst_cell){
 
-offset_array = text_vect.positions;
+    // get triangle verts
+    vert_1 = text_vect.positions[inst_cell[0]]
+    vert_2 = text_vect.positions[inst_cell[1]]
+    vert_3 = text_vect.positions[inst_cell[2]]
+
+    // merge verts into position vector
+    inst_verts = [vert_1, vert_2, vert_3];
+
+    tri_verts.push(inst_verts);
+
+  });
 
 const draw = regl({
+  vert: `
+  precision mediump float;
+
+  // These three are instanced attributes.
+  attribute vec2 offset;
+  attribute vec2 position;
+
+  void main() {
+    gl_Position = vec4(
+         position[0] + position[1],
+        -position[0] + position[1],
+        0,
+        1);
+  }`,
+
   frag: `
   precision mediump float;
 
@@ -31,28 +54,19 @@ const draw = regl({
     gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
   }`,
 
-  vert: `
-  precision mediump float;
-
-  attribute vec2 position;
-
-  // These three are instanced attributes.
-  attribute vec3 color;
-  attribute vec2 offset;
-
-  void main() {
-    gl_Position = vec4(
-         position.x + position.y + offset.x,
-        -position.x + position.y + offset.y, 0, 1);
-  }`,
-
   attributes: {
+
     position: [[0.0, -0.05], [-0.05, 0.0], [0.05, 0.05]],
 
-    offset: {
-      buffer: regl.buffer(offset_array),
-      divisor: 1 // one separate offset for every triangle.
-    },
+    // position: {
+    //   buffer: regl.buffer(tri_verts),
+    //   divisor: 1 //
+    // },
+
+    // offset: {
+    //   buffer: regl.buffer(text_vect.positions),
+    //   divisor: 1 // one separate offset for every triangle.
+    // },
 
   },
 
