1703dee6d9de3e8fee03abf01e62898228d1e54f
cornhundred <nickfloresfernandez@gmail.com>
2017-06-26 17:59:04 -0400

    moved draw rows to separate function, passing same regl instance over to get zoom-context working

--- draw_rows.js
+++ draw_rows.js
@@ -1,84 +1,16 @@
-/*
-  tags: instancing, basic
-
-  In this example, it is shown how you can draw a bunch of triangles using the
-  instancing feature of regl.
-
- */
-
 const regl = require('regl')({extensions: ['angle_instanced_arrays']})
-
 var m3 = require('./mat3_transform');
+
+var draw_rows_fun = require('./draw_rows_fun');
+
+var draw_rows = draw_rows_fun(regl);
 
 const camera = require('./camera-2d')(regl, {
   xrange: [-1.5, 1.5],
   yrange: [-1.5, 1.5]
 });
 
-var zoom_function = function(context){
-  return context.view;
-}
-
 window.addEventListener('resize', camera.resize);
-
-mat_scale = m3.scaling(0.1, 0.1);
-mat_rotate = m3.rotation(Math.PI/4);
-vec_translate = [-0.5, 0.5, 0.0];
-
-// draw background
-const draw_rows = regl({
-
-  vert: `
-    precision highp float;
-    attribute vec2 position;
-    varying vec3 new_position;
-    uniform mat3 mat_rotate;
-    uniform mat3 mat_scale;
-    uniform vec3 vec_translate;
-    uniform mat4 zoom;
-
-    void main () {
-
-      // zoom multiplication does zoom
-
-      new_position = vec3(position, 0);
-
-      // rotate, zoom and then translate
-      new_position = mat_rotate * mat_scale * new_position + vec_translate;
-
-      gl_Position = zoom * vec4(new_position, 1);
-
-    }
-  `,
-
-  frag: `
-
-    // color triangle red
-    void main () {
-      gl_FragColor = vec4(0, 0, 0, 1);
-    }
-
-  `,
-
-  attributes: {
-    position: [
-      [-0.5, 0.5],
-      [-0.5, -0.5],
-      [0.5, -0.5],
-    ]
-  },
-
-  uniforms: {
-    zoom: zoom_function,
-    mat_rotate: mat_rotate,
-    mat_scale: mat_scale,
-    vec_translate: vec_translate
-  },
-
-  count: 3
-
-});
-
 
 regl.frame(function () {
 
