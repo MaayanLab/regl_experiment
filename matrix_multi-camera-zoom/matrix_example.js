dbae6728c614313a389077c3008b351017012c95
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-10-19 17:17:57 -0400

    making 2d matrix data

--- matrix_multi-camera-zoom/matrix_example.js
+++ matrix_multi-camera-zoom/matrix_example.js
@@ -5,22 +5,28 @@
 
 const regl = require('regl')({extensions: ['angle_instanced_arrays']})
 
-var num_cell = 100;
+var num_cell = 30;
 
 console.log('multi-camera-zooming, passing in opacity')
 
 var draw_mat_rows = require('./draw_mat_labels')(regl, num_cell, 'row');
 var draw_mat_cols = require('./draw_mat_labels')(regl, num_cell, 'col');
 
-opacity_data = []
-  for (var i = 0; i < num_cell * num_cell; i++) {
-    opacity_data[i] = Math.random();
+mat_data = []
+for (var i=0; i < num_cell; i++){
+  mat_data[i] = []
+  for (var j=0; j < num_cell; j++){
+    mat_data[i][j] = Math.random();
   }
+}
+
+// flat_mat = [].concat.apply([], mat_data);
+console.log(mat_data.length, mat_data[0].length)
 
 var num_row = num_cell;
 var num_col = num_cell;
 
-var draw_cells = require('./draw_cells')(regl, num_row, num_col, opacity_data);
+var draw_cells = require('./draw_cells')(regl, num_row, num_col, mat_data);
 
 const camera_1 = require('./camera_1')(regl, {
   xrange: [-1.5, 1.5],
