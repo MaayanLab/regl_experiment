90203d4b919362126674311aca37020ad416ca10
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-10-19 15:35:07 -0400

    working on making normal zoom example with a matrix

---
+++ matrix_normal-zoom/matrix_example.js
@@ -0,0 +1,39 @@
+/*
+  Making an interactive matrix using instancing.
+
+ */
+
+const regl = require('regl')({extensions: ['angle_instanced_arrays']})
+
+var num_cell = 1000;
+console.log('here')
+
+var draw_mat_rows = require('./draw_mat_labels')(regl, num_cell, 'row');
+var draw_mat_cols = require('./draw_mat_labels')(regl, num_cell, 'col');
+var draw_cells = require('./draw_cells')(regl, num_cell);
+
+const camera = require('./camera-2d')(regl, {
+  xrange: [-1.5, 1.5],
+  yrange: [-1.5, 1.5]
+});
+
+window.addEventListener('resize', camera.resize);
+
+regl.frame(function () {
+
+  camera.draw(() => {
+
+    // clear the background
+    regl.clear({
+      color: [0, 0, 0, 0]
+    });
+
+    // draw two parts of the matrix cell
+    draw_cells.top();
+    draw_cells.bot();
+    draw_mat_rows();
+    draw_mat_cols();
+
+  });
+
+})
