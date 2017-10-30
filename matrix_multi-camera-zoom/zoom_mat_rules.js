0628c6d557261da483fc04da49a6e7c79685ade2
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-10-30 10:28:22 -0400

    cleaning up zoom_mat_rules

--- matrix_multi-camera-zoom/zoom_mat_rules.js
+++ matrix_multi-camera-zoom/zoom_mat_rules.js
@@ -21,90 +21,66 @@ module.exports = function(regl){
 
   interactionEvents({
     element: element,
-  }).on('interaction', function (ev) {
-
+  }).on('interaction', function(ev, zoom_info){
     if (ev.buttons || interaction_types.indexOf(ev.type) !== -1)  {
-
-      switch (ev.type) {
-        case 'wheel':
-          ev.dsx = ev.dsy = Math.exp(-ev.dy / 100);
-          ev.dx = ev.dy = 0;
-          break;
-      }
-
-      zoom_info.dsx = ev.dsx;
-      zoom_info.dsy = ev.dsy;
-      zoom_info.dx = ev.dx;
-      zoom_info.dy = ev.dy;
-      zoom_info.x0 = ev.x0;
-      zoom_info.y0 = ev.y0;
-
-      // // X zooming rules
-      // //////////////////////
-      // // zooming within allowed range
-      // if (zoom_info.tsx < max_zoom && zoom_info.tsx > min_zoom){
-      //   zoom_info.tsx = zoom_info.tsx * ev.dsx;
-      // }
-      // // above max zoom (can only go down)
-      // else if (zoom_info.tsx >= max_zoom) {
-      //   if (zoom_info.dsx < 1){
-      //     zoom_info.tsx = zoom_info.tsx * ev.dsx;
-      //   }
-      // }
-      // // below min zoom (can only go up)
-      // else if (zoom_info.tsx <= min_zoom){
-      //   if (zoom_info.dsx > 1){
-      //     zoom_info.tsx = zoom_info.tsx * ev.dsx;
-      //   }
-      // }
-
-      // Y zooming rules
-      ////////////////////////////
-      // var inst_axis = 'y'
-
-      _.each(['x', 'y'], function(inst_axis){
-
-        var inst_ts = 'ts' + inst_axis;
-        var inst_ds = 'ds' + inst_axis;
-
-        // zooming within allowed range
-        if (zoom_info[inst_ts] < max_zoom && zoom_info[inst_ts] > min_zoom){
-          zoom_info[inst_ts] = zoom_info[inst_ts] * ev[inst_ds];
-        }
-        else if (zoom_info[inst_ts] >= max_zoom) {
-
-          if (zoom_info[inst_ds] < 1){
-            zoom_info[inst_ts] = zoom_info[inst_ts] * ev[inst_ds];
-          } else {
-            zoom_info[inst_ds] = max_zoom/zoom_info[inst_ts];
-            zoom_info[inst_ts] = max_zoom;
-
-          }
-        }
-        // below min zoom (can only go up)
-        else if (zoom_info[inst_ts] <= min_zoom){
-
-          if (zoom_info[inst_ds] > 1){
-            zoom_info[inst_ts] = zoom_info[inst_ts] * ev[inst_ds];
-          } else {
-            zoom_info[inst_ds] = min_zoom/zoom_info[inst_ts];
-            zoom_info[inst_ts] = min_zoom;
-          }
-        }
-
-      });
-
-      // interaction rules
-      /////////////////////
-      if (still_interacting == false){
-        still_interacting = true;
-        setTimeout(function(){
-          return still_interacting = false;
-        }, 1000)
-      }
-
+      restrict_zooming(ev);
     }
-  })
+  }
+  )
+
+  function restrict_zooming(ev) {
+
+    switch (ev.type) {
+      case 'wheel':
+        ev.dsx = ev.dsy = Math.exp(-ev.dy / 100);
+        ev.dx = ev.dy = 0;
+        break;
+    }
+
+    zoom_info.dsx = ev.dsx;
+    zoom_info.dsy = ev.dsy;
+    zoom_info.dx = ev.dx;
+    zoom_info.dy = ev.dy;
+    zoom_info.x0 = ev.x0;
+    zoom_info.y0 = ev.y0;
+
+    // X and Y zooming rules
+    _.each(['x', 'y'], function(inst_axis){
+
+      var inst_ts = 'ts' + inst_axis;
+      var inst_ds = 'ds' + inst_axis;
+
+      // zooming within allowed range
+      if (zoom_info[inst_ts] < max_zoom && zoom_info[inst_ts] > min_zoom){
+        zoom_info[inst_ts] = zoom_info[inst_ts] * ev[inst_ds];
+      }
+      else if (zoom_info[inst_ts] >= max_zoom) {
+        if (zoom_info[inst_ds] < 1){
+          zoom_info[inst_ts] = zoom_info[inst_ts] * ev[inst_ds];
+        } else {
+          zoom_info[inst_ds] = max_zoom/zoom_info[inst_ts];
+          zoom_info[inst_ts] = max_zoom;
+        }
+      }
+      else if (zoom_info[inst_ts] <= min_zoom){
+        if (zoom_info[inst_ds] > 1){
+          zoom_info[inst_ts] = zoom_info[inst_ts] * ev[inst_ds];
+        } else {
+          zoom_info[inst_ds] = min_zoom/zoom_info[inst_ts];
+          zoom_info[inst_ts] = min_zoom;
+        }
+      }
+
+    });
+
+    if (still_interacting == false){
+      still_interacting = true;
+      setTimeout(function(){
+        return still_interacting = false;
+      }, 1000)
+    }
+
+  }
 
   return zoom_info;
 
