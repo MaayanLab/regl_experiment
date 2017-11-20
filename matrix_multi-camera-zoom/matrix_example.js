a03b8e7211b4263d95ffac8ea318a6ee0698ef21
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-11-20 14:12:15 -0500

    cleaned up viz_dim definition

--- matrix_multi-camera-zoom/matrix_example.js
+++ matrix_multi-camera-zoom/matrix_example.js
@@ -158,7 +158,37 @@ function run_viz(regl, assets){
   zoom_restrict_mat.y.min = 1.0;
   zoom_restrict_mat.y.ratio = 1;
 
-  zoom_rules_high_mat(regl, zoom_restrict_mat, zoom_data, 'mat');
+
+  // Set up viz_dim
+  ///////////////////////
+  var opts = opts || {};
+  var options = extend({
+      element: opts.element || regl._gl.canvas,
+    }, opts || {});
+
+  var element = options.element;
+
+  var viz_dim = {};
+  viz_dim.canvas = {};
+  viz_dim.mat = {};
+
+  _.each(['width', 'height'], function(inst_dim){
+    viz_dim.canvas[inst_dim] = Number.parseFloat(d3.select(element)
+      .style(inst_dim).replace('px', ''));
+  });
+
+  // square matrix size set by width of canvas
+  viz_dim.mat.width = viz_dim.canvas.width/2;
+  viz_dim.mat.height = viz_dim.canvas.width/2;
+
+  // min and max position of matrix
+  viz_dim.mat.min_x = viz_dim.canvas.width/2 - viz_dim.mat.width/2;
+  viz_dim.mat.max_x = viz_dim.canvas.width/2 + viz_dim.mat.width/2;
+
+  viz_dim.mat.min_y = viz_dim.canvas.height/2 - viz_dim.mat.height/2;
+  viz_dim.mat.max_y = viz_dim.canvas.height/2 + viz_dim.mat.height/2;
+
+  zoom_rules_high_mat(regl, zoom_restrict_mat, zoom_data, 'mat', viz_dim);
 
   var zoom_infos = {}
   zoom_infos['row-labels'] = zoom_rules['row-labels'](regl, zoom_restrict, 'row-labels');
--- matrix_multi-camera-zoom/zoom_rules_high_mat.js
+++ matrix_multi-camera-zoom/zoom_rules_high_mat.js
@@ -2,7 +2,7 @@ var interactionEvents = require('interaction-events');
 var extend = require('xtend/mutable');
 var restrict_zoom_on_interaction = require('./restrict_zoom_on_interaction');
 
-module.exports = function zoom_rules_high_mat(regl, zoom_restrict, zoom_data, viz_dim, viz_component){
+module.exports = function zoom_rules_high_mat(regl, zoom_restrict, zoom_data, viz_component, viz_dim){
 
   var opts = opts || {};
   var options = extend({
@@ -10,26 +10,6 @@ module.exports = function zoom_rules_high_mat(regl, zoom_restrict, zoom_data, vi
     }, opts || {});
 
   var element = options.element;
-
-  var viz_dim = {};
-  viz_dim.canvas = {};
-  viz_dim.mat = {};
-
-  _.each(['width', 'height'], function(inst_dim){
-    viz_dim.canvas[inst_dim] = Number.parseFloat(d3.select(element)
-      .style(inst_dim).replace('px', ''));
-  });
-
-  // square matrix size set by width of canvas
-  viz_dim.mat.width = viz_dim.canvas.width/2;
-  viz_dim.mat.height = viz_dim.canvas.width/2;
-
-  // min and max position of matrix
-  viz_dim.mat.min_x = viz_dim.canvas.width/2 - viz_dim.mat.width/2;
-  viz_dim.mat.max_x = viz_dim.canvas.width/2 + viz_dim.mat.width/2;
-
-  viz_dim.mat.min_y = viz_dim.canvas.height/2 - viz_dim.mat.height/2;
-  viz_dim.mat.max_y = viz_dim.canvas.height/2 + viz_dim.mat.height/2;
 
   global_translate = 0
   lock_left = false
