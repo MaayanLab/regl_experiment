1d521c70886603b7eff2df171f344cd754d973c0
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-11-20 15:18:03 -0500

    working on right side pan restriction

--- matrix_multi-camera-zoom/zoom_rules_low_mat.js
+++ matrix_multi-camera-zoom/zoom_rules_low_mat.js
@@ -1,7 +1,10 @@
 
 module.exports = function zoom_rules_low_mat(zoom_restrict, zoom_data, viz_dim_mat, viz_component){
 
-  // X Zooming Rules
+  /////////////////////////
+  // Zooming Rules
+  /////////////////////////
+
   var max_zoom = zoom_restrict.max;
   var min_zoom = zoom_restrict.min;
 
@@ -14,7 +17,7 @@ module.exports = function zoom_rules_low_mat(zoom_restrict, zoom_data, viz_dim_m
     zoom_data.total_zoom = potential_total_zoom;
   }
 
-  // zoom above allowed range
+  // Zoom above max
   else if (potential_total_zoom >= max_zoom) {
     if (zoom_data.inst_zoom < 1){
       zoom_data.total_zoom = zoom_data.total_zoom * zoom_data.inst_zoom;
@@ -25,16 +28,21 @@ module.exports = function zoom_rules_low_mat(zoom_restrict, zoom_data, viz_dim_m
       zoom_data.total_zoom = max_zoom;
     }
   }
+  // Zoom below min
   else if (potential_total_zoom <= min_zoom){
     if (zoom_data.inst_zoom > 1){
       zoom_data.total_zoom = zoom_data.total_zoom * zoom_data.inst_zoom;
     } else {
+      // bump zoom up to max
       zoom_data.inst_zoom =  min_zoom/zoom_data.total_zoom;
+      // set zoom to max
       zoom_data.total_zoom = min_zoom;
     }
   }
 
-  var zoom_eff = 1 - zoom_data.inst_zoom;
+  //////////////////////////////////
+  // Pan Rules
+  //////////////////////////////////
 
   // restrict positive pan_by_drag if necessary
   if (zoom_data.pan_by_drag > 0){
@@ -54,8 +62,7 @@ module.exports = function zoom_rules_low_mat(zoom_restrict, zoom_data, viz_dim_m
   // tracking cursor offset (working)
   var cursor_relative_axis = zoom_data.cursor_position - viz_dim_mat.min;
 
-  // negative cursor offsets are set to zero
-  // (cannot zoom with cursor to left of matrix)
+  // restrict cursor_relative_axis
   if (cursor_relative_axis < 0){
     cursor_relative_axis = 0;
   } else if (cursor_relative_axis > viz_dim_mat.max){
@@ -63,6 +70,7 @@ module.exports = function zoom_rules_low_mat(zoom_restrict, zoom_data, viz_dim_m
   }
 
   // pan by zoom relative to the axis
+  var zoom_eff = 1 - zoom_data.inst_zoom;
   zoom_data.pbz_relative_axis = zoom_eff * cursor_relative_axis;
 
   var potential_total_pan = zoom_data.total_pan +
@@ -70,30 +78,21 @@ module.exports = function zoom_rules_low_mat(zoom_restrict, zoom_data, viz_dim_m
                  zoom_data.pbz_relative_axis / zoom_data.total_zoom ;
 
 
+  // Panning in bounds
   if (potential_total_pan <= 0.0001){
 
     zoom_data.pan_by_zoom = zoom_eff * zoom_data.cursor_position;
-
     zoom_data.total_pan = potential_total_pan;
 
   } else {
 
-    /*
-    reposition total_pan to the left, and use the camera to bump it over
-    */
 
-    //////////////////////////////////////
-    // pan-by-zoom restriction seems to be
-    // working when zooming out with mouse
-    // (zoom in, pan right, zoom out)
-    //////////////////////////////////////
+    // push over by total_pan (negative value) times total zoom applied
+    // since need to push more when matrix has been effectively increased in
+    // size
+    var push_matrix = zoom_data.total_pan * zoom_data.total_zoom;
 
-    // push over by total pan (negative position) times total zoom applied
-    // (since need to push more when matrix has been effectively increased in
-    // size)
-    var push_by_total_pan = zoom_data.total_pan * zoom_data.total_zoom;
-
-    zoom_data.pan_by_zoom = zoom_eff * viz_dim_mat.min - push_by_total_pan;
+    zoom_data.pan_by_zoom = zoom_eff * viz_dim_mat.min - push_matrix;
     zoom_data.total_pan = 0
 
   }
